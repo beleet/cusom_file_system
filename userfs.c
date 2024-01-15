1638f8bacf943c818d2fe14570e049875338be05
philipp3388 <71440514+philipp3388@users.noreply.github.com>
2024-01-15 23:52:08 +0300

    open operations passed

--- userfs.c
+++ userfs.c
@@ -1,5 +1,7 @@
 #include "userfs.h"
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 enum {
     BLOCK_SIZE = 512,
@@ -66,18 +68,97 @@ ufs_errno()
     return ufs_error_code;
 }
 
+// Searching file by the file name
+struct file* search_file(const char* file_name) {
+    struct file* current_file = file_list;
+
+    while (current_file != NULL) {
+        if (current_file->name == file_name)
+            return current_file;
+        current_file = current_file->next;
+    }
+
+    return NULL;
+}
+
+int create_file_descriptor(struct file *file) {
+
+    struct filedesc *new_file_descriptor = (struct filedesc*) calloc(1, sizeof(struct filedesc));
+
+    new_file_descriptor->file = file;
+
+    //  Initialize the file_descriptors array if its empty
+    if (file_descriptor_capacity == 0) {
+        file_descriptor_capacity = 1;
+        file_descriptors = (struct filedesc **) calloc(1, sizeof(struct filedesc*));
+    }
+
+    //  Looking for free cell for new created descriptor
+    for (int i = 0; i < file_descriptor_capacity; i++) {
+        if (file_descriptors[i] == NULL) {
+            file_descriptors[i] = new_file_descriptor;
+            return i;
+        }
+    }
+
+    //  Logic that handles the case when array of descriptors is full
+    file_descriptor_capacity *= 2;
+    struct filedesc **extended_file_descriptors = (struct filedesc **) calloc(file_descriptor_capacity, sizeof(struct filedesc*));
+
+    for (int i = 0; i < file_descriptor_capacity / 2; i++)
+        extended_file_descriptors[i] = file_descriptors[i];
+
+    free(file_descriptors);
+
+    extended_file_descriptors[file_descriptor_capacity / 2] = new_file_descriptor;
+    file_descriptors = extended_file_descriptors;
+
+    return file_descriptor_capacity / 2;
+}
+
+struct file *create_file(const char *filename) {
+
+    struct file *new_file = (struct file*) calloc(1, sizeof(struct file));
+    new_file->name = (char *) filename;
+
+    if (file_list == NULL)
+        file_list = new_file;
+
+    else {
+        struct file *index_file = file_list;
+
+        while (index_file->next != NULL)
+            index_file = index_file->next;
+
+        index_file = new_file;
+    }
+
+    return new_file;
+}
+
 int
 ufs_open(const char *filename, int flags)
 {
-    /* IMPLEMENT THIS FUNCTION */
-    (void)filename;
-    (void)flags;
-    (void)file_list;
-    (void)file_descriptors;
-    (void)file_descriptor_count;
-    (void)file_descriptor_capacity;
-    ufs_error_code = UFS_ERR_NOT_IMPLEMENTED;
-    return -1;
+    ufs_error_code = UFS_ERR_NO_ERR;
+
+    struct file *desired_file = search_file(filename);
+
+    if (flags == 0) {
+
+        if (desired_file == NULL) {
+            ufs_error_code = UFS_ERR_NO_FILE;
+            return -1;
+        }
+    }
+
+    if (flags == UFS_CREATE) {
+
+        if (desired_file == NULL) {
+            desired_file = create_file(filename);
+        }
+    }
+
+    return create_file_descriptor(desired_file);;
 }
 
 ssize_t
@@ -105,19 +186,32 @@ ufs_read(int fd, char *buf, size_t size)
 int
 ufs_close(int fd)
 {
-    /* IMPLEMENT THIS FUNCTION */
-    (void)fd;
-    ufs_error_code = UFS_ERR_NOT_IMPLEMENTED;
-    return -1;
+    file_descriptors[fd] = NULL;
+    return 0;
 }
 
 int
 ufs_delete(const char *filename)
 {
-    /* IMPLEMENT THIS FUNCTION */
-    (void)filename;
-    ufs_error_code = UFS_ERR_NOT_IMPLEMENTED;
-    return -1;
+    struct file *temp = file_list, *prev = NULL;
+
+    while (temp != NULL && temp->name != filename) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return -1;
+
+    if (prev == NULL) {
+        file_list = temp->next;
+    } else {
+        prev->next = temp->next;
+    }
+
+    free(temp);
+
+    return 0;
 }
 
 void
